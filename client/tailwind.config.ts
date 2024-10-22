41d73c9f1fe124d03a27ef37c394dfef9367befb
cofeek-codes <kormyshev11@mail.ru>
2024-10-22 22:34:11 +0500

    tailwind custom theme implemented
    tailwind theme implemented; fonts loaded; fonts tested;

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,19 +1,31 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-};
-export default config;
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			screens: {
+				xl: '1920px',
+				lg: '1080px',
+			},
+			colors: {
+				blue: '#dcdcf0',
+				white: '#3553a1',
+				purple: '#260047',
+				darkPurple: '0a0019',
+				background: 'var(--background)',
+				foreground: 'var(--foreground)',
+			},
+			fontFamily: {
+				regular: ['Euclid Circular B Regular'],
+				bold: ['Euclid Circular B Bold'],
+			},
+		},
+	},
+	plugins: [],
+}
+export default config
