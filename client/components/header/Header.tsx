c8a65582d74de4a0a1ec147e2ef73c1930f79221
cofeek-codes <kormyshev11@mail.ru>
2024-12-14 18:14:55 +0500

    [Header]: logo link fixed

--- client/components/header/Header.tsx
+++ client/components/header/Header.tsx
@@ -7,9 +7,24 @@ import logo from '/assets/logo.png'
 
 import Cookies from 'js-cookie'
 import { useRouter } from 'next/navigation'
+import { isAdmin, isStudent, isTeacher } from '@/utils/roleChecker'
+import { useEffect, useState } from 'react'
 
 const Header = () => {
 	const router = useRouter()
+	const [logoLink, setLogoLink] = useState('')
+	useEffect(() => {
+		if (isStudent()) {
+			setLogoLink('/profile/student')
+		} else if (isTeacher()) {
+			setLogoLink('/profile/teacher')
+		} else if (isAdmin()) {
+			setLogoLink('/admin')
+		} else {
+			setLogoLink('/auth')
+		}
+	}, [])
+
 	function handleLogout(e: any) {
 		e.preventDefault()
 		Cookies.remove('user_id')
@@ -20,7 +35,7 @@ const Header = () => {
 	return (
 		<header className='flex w-full justify-between bg-purple items-center left-0'>
 			<div className='header__logo my-[8px] ml-[25px]'>
-				<Link href={'/profile'}>
+				<Link href={logoLink}>
 					<Image src={logo} width={102} height={74} alt='logo' />
 				</Link>
 			</div>
