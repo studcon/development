722a20f54b38e54cb5ba03cd49c480e86b91a146
cofeek-codes <kormyshev11@mail.ru>
2024-12-26 20:10:17 +0500

    [dynamic avatars]: photo preview in update modal

--- client/components/modal/UpdateUserModal.tsx
+++ client/components/modal/UpdateUserModal.tsx
@@ -1,45 +1,37 @@
 'use client'
 
 import { IGroup } from '@/types/models/IGroup'
-import { IUser } from '@/types/models/IUser'
 import axiosInstance from '@/utils/axiosInstance'
 import { AxiosError } from 'axios'
 import { useRouter } from 'next/navigation'
+import defaultAvatar from '/assets/avatar.png'
 import { Dispatch, SetStateAction, useEffect, useState } from 'react'
 import Select, { StylesConfig } from 'react-select'
 import { Bounce, toast } from 'react-toastify'
 import Modal from './Modal'
+import { IUser } from '@/types/models/IUser'
+import Image from 'next/image'
+import { AVATARS_URL } from '@/constants'
 
-interface IUpdateProjectModal {
+interface ICreateModal {
 	isModalOpen: boolean
 	setModalOpen: Dispatch<SetStateAction<boolean>>
-	userId: number
 	update: boolean
 	setUpdate: Dispatch<SetStateAction<boolean>>
 	user: IUser
 }
 
-type FormData = {
-	name: string
-	surname: string
-	patronymic: string
-	login: string
-	password: string
-	group_id: number
-	role_id: number
-}
+type FormState = Omit<IUser, 'photo'> & { password: string; photoFile: File } // doing photoFile separately to send it
 
-const UpdateUserModal = (props: IUpdateProjectModal) => {
-	const [formData, setFormData] = useState<FormData>({} as FormData)
-	const [projectData, setProjectData] = useState<any>()
+const CreateUserModal = (props: ICreateModal) => {
+	const [formState, setFormState] = useState<FormState>({} as FormState)
 	const [error, setError] = useState<any>(null)
 	const [groups, setGroups] = useState<any>(null)
-	const [groupOptions, setGroupOptions] = useState<any>(null)
+	const [groupOptions, setGroupOptions] = useState<IGroup[]>([])
 	const roleOptions = [
 		{ value: 1, label: 'Студент' },
 		{ value: 2, label: 'Преподаватель' },
 	]
-	const router = useRouter()
 
 	const selectStyles: StylesConfig = {
 		control: styles => ({
@@ -53,6 +45,8 @@ const UpdateUserModal = (props: IUpdateProjectModal) => {
 			...styles,
 			backgroundColor: '#0a0019',
 			border: 'none',
+			marginTop: '10px',
+			marginBottom: '20px',
 		}),
 
 		option: styles => ({
@@ -74,29 +68,30 @@ const UpdateUserModal = (props: IUpdateProjectModal) => {
 		}),
 	}
 
+	const router = useRouter()
 	useEffect(() => {
-		axiosInstance.get(`/admin/getGroups`).then(res => {
+		axiosInstance.get('/admin/getGroups').then(res => {
+			console.log(res.data.message)
 			setGroups(res.data.message)
-
 			setGroupOptions(
-				res.data.message.map((g: IGroup) => ({ value: g.id, label: g.name }))
+				res.data.message.map((g: IGroup) => ({ value: g.id, label: g.name })),
 			)
 		})
-	}, [props.update])
-	function updateUser(e: any) {
+	}, [])
+	function submitUser(e: any) {
 		e.preventDefault()
-		console.log(formData)
+		console.log(formState)
+		let formData = new FormData()
+		formData.append('name', formState.name)
+		formData.append('surname', formState.surname)
+		formData.append('patronymic', formState.patronymic)
+		formData.append('photo', formState.photoFile)
+		formData.append('login', formState.login)
+		formData.append('password', formState.password)
+		formData.append('role_id', formState.role_id.toString())
+		formData.append('group_id', formState.group_id.toString())
 		axiosInstance
-			.put(`/admin/updateUser/`, {
-				id: props.userId,
-				name: formData.name,
-				surname: formData.surname,
-				patronymic: formData.patronymic,
-				login: formData.login,
-				password: formData.password,
-				group_id: formData.group_id,
-				role_id: formData.role_id,
-			})
+			.post('/admin/updateUser', formData)
 			.then(res => {
 				props.setUpdate(!props.update)
 				if (res.data.code != 200) {
@@ -130,7 +125,7 @@ const UpdateUserModal = (props: IUpdateProjectModal) => {
 			<div className='flex flex-col justify-between h-full'>
 				<div>
 					<h2 className='text-center text-[30px] mb-[15px]'>
-						Изменение пользователя
+						Добавление пользователя
 					</h2>
 					<form action='flex flex-col'>
 						<label className='text-[25px] ml-2.5' htmlFor='title__input'>
@@ -138,12 +133,12 @@ const UpdateUserModal = (props: IUpdateProjectModal) => {
 						</label>
 						<input
 							onInput={(e: any) => {
-								setFormData({ ...formData, name: e.target.value })
+								setFormState({ ...formState, name: e.target.value })
 							}}
-							defaultValue={props.user.name}
 							type='text'
 							id='title__input'
 							maxLength={120}
+							defaultValue={props.user.name}
 							className='h-[60px] w-full text-[25px] px-[15px] py-[10px] mt-[10px] mb-[20px] rounded-[10px] bg-white text-black'
 							required
 						/>
@@ -153,12 +148,12 @@ const UpdateUserModal = (props: IUpdateProjectModal) => {
 						</label>
 						<input
 							onInput={(e: any) => {
-								setFormData({ ...formData, surname: e.target.value })
+								setFormState({ ...formState, surname: e.target.value })
 							}}
-							defaultValue={props.user.surname}
 							type='text'
 							id='descroption__input'
 							maxLength={500}
+							defaultValue={props.user.surname}
 							className='h-[60px] w-full text-[25px] px-[15px] py-[10px] mt-[10px] mb-[20px] rounded-[10px] bg-white text-black'
 							required
 						/>
@@ -168,12 +163,12 @@ const UpdateUserModal = (props: IUpdateProjectModal) => {
 						</label>
 						<input
 							onInput={(e: any) => {
-								setFormData({ ...formData, patronymic: e.target.value })
+								setFormState({ ...formState, patronymic: e.target.value })
 							}}
-							defaultValue={props.user.patronymic}
 							type='text'
 							id='descroption__input'
 							maxLength={500}
+							defaultValue={props.user.patronymic}
 							className='h-[60px] w-full text-[25px] px-[15px] py-[10px] mt-[10px] mb-[20px] rounded-[10px] bg-white text-black'
 							required
 						/>
@@ -183,12 +178,12 @@ const UpdateUserModal = (props: IUpdateProjectModal) => {
 						</label>
 						<input
 							onInput={(e: any) => {
-								setFormData({ ...formData, login: e.target.value })
+								setFormState({ ...formState, login: e.target.value })
 							}}
-							defaultValue={props.user.login}
 							type='text'
 							id='descroption__input'
 							maxLength={500}
+							defaultValue={props.user.login}
 							className='h-[60px] w-full text-[25px] px-[15px] py-[10px] mt-[10px] mb-[20px] rounded-[10px] bg-white text-black'
 							required
 						/>
@@ -198,7 +193,7 @@ const UpdateUserModal = (props: IUpdateProjectModal) => {
 						</label>
 						<input
 							onInput={(e: any) => {
-								setFormData({ ...formData, password: e.target.value })
+								setFormState({ ...formState, password: e.target.value })
 							}}
 							type='password'
 							id='descroption__input'
@@ -213,10 +208,13 @@ const UpdateUserModal = (props: IUpdateProjectModal) => {
 						<Select
 							onChange={(e: any) => {
 								console.log(e)
-								setFormData({ ...formData, group_id: e.value })
+								setFormState({ ...formState, group_id: e.value })
 							}}
 							styles={selectStyles}
-							// defaultValue={groupOptions[0]}
+							// @TODO: validate groups are loaded
+							defaultValue={groupOptions.find(
+								e => e.id === props.user.group_id,
+							)}
 							options={groupOptions}
 						/>
 						<label className='text-[25px] ml-2.5' htmlFor='descroption__input'>
@@ -225,17 +223,55 @@ const UpdateUserModal = (props: IUpdateProjectModal) => {
 						<Select
 							onChange={(e: any) => {
 								console.log(e)
-								setFormData({ ...formData, role_id: e.value })
+								setFormState({ ...formState, role_id: e.value })
 							}}
 							styles={selectStyles}
+							// @TODO: validate groups are loaded
+							defaultValue={roleOptions.find(
+								e => e.value === props.user.role_id,
+							)}
 							options={roleOptions}
+						/>
+
+						<label className='text-[25px] ml-2.5' htmlFor='descroption__input'>
+							Фото
+						</label>
+						<div className='mt-[10px] mb-[20px]'>
+							<Image
+								src={
+									props.user.photo
+										? AVATARS_URL + props.user.photo
+										: defaultAvatar
+								}
+								className='rounded-[10px]'
+								width={180}
+								height={180}
+								alt='avatar'
+							/>
+						</div>
+						<label
+							htmlFor='photo__input'
+							className='block w-fit h-fit mt-[10px] mb-[20px] bg-lightPurple rounded-[22px] py-[14px] px-[35px] hover:bg-buttonsHover hover:transition-[0.3s] transition-[0.3s]'
+						>
+							<span className='text-[20px] font-regular'>
+								{formState.photoFile?.name || 'Выбрать фото'}
+							</span>
+						</label>
+						<input
+							id='photo__input'
+							type='file'
+							className='hidden'
+							accept='image/*'
+							onChange={e => {
+								setFormState({ ...formState, photoFile: e.target.files![0] })
+							}}
 						/>
 					</form>
 				</div>
 				<div className='self-end my-[25px] pb-[25px]'>
 					<button
 						onClick={e => {
-							updateUser(e)
+							submitUser(e)
 						}}
 						className='bg-lightPurple rounded-[22px] py-[14px] px-[35px] mr-[15px] hover:bg-buttonsHover hover:transition-[0.3s] transition-[0.3s]'
 					>
@@ -253,4 +289,4 @@ const UpdateUserModal = (props: IUpdateProjectModal) => {
 	)
 }
 
-export default UpdateUserModal
+export default CreateUserModal
