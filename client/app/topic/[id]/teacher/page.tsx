77129468e286829f796580ba7e408e54f9a2a687
cofeek-codes <kormyshev11@mail.ru>
2025-01-02 05:58:24 +0500

    [TestUpdate]:  opening test editor on "edit" button

--- client/app/topic/[id]/teacher/page.tsx
+++ client/app/topic/[id]/teacher/page.tsx
@@ -17,189 +17,199 @@ import TestEditor from '@/components/teacher/topic/TestEditor'
 import BreadCrumps from '@/components/BreadCrumps'
 
 const Topic = () => {
-	const [isLectionCreateOpen, setLectionCreateOpen] = useState<boolean>(false)
-	const [isLectionUpdateOpen, setLectionUpdateOpen] = useState<boolean>(false)
-	const [lectionUpdate, setLectionUpdate] = useState(null)
-	const [isTestCreateOpen, setTestCreateOpen] = useState<boolean>(false)
-	const [topic, setTopic] = useState<any>(null)
-	const [error, setError] = useState<AxiosError | null>(null)
-	const [tabs, setTabs] = useState<React.JSX.Element[]>([])
+   const [isLectionCreateOpen, setLectionCreateOpen] = useState<boolean>(false)
+   const [isLectionUpdateOpen, setLectionUpdateOpen] = useState<boolean>(false)
+   const [lectionUpdate, setLectionUpdate] = useState(null)
+   const [isTestCreateOpen, setTestCreateOpen] = useState<boolean>(false)
+   const [isTestUpdateOpen, setTestUpdateOpen] = useState<boolean>(false)
+   const [topic, setTopic] = useState<any>(null)
+   const [error, setError] = useState<AxiosError | null>(null)
+   const [tabs, setTabs] = useState<React.JSX.Element[]>([])
 
-	const router = useRouter()
-	const params = useParams()
+   const router = useRouter()
+   const params = useParams()
 
-	const getMaterials = () => {
-		console.log('get')
+   const getMaterials = () => {
+      console.log('get')
 
-		axiosInstance
-			.get(`/topic/getMaterials/${params.id}`)
-			.then(res => {
-				setTopic(res.data.message)
-				setTabs([])
-				console.log(res.data.message)
-				res.data.message.lectures.forEach((lecture: ILecture) => {
-					setTabs(tabs => [
-						...tabs,
-						<Lection
-							lecture={lecture}
-							SetLectionUpdateOpen={setLectionUpdateOpen}
-							setLectionUpdate={setLectionUpdate}
-							isTest={false}
-							getMaterials={getMaterials}
-						/>,
-					])
-				})
-				res.data.message.tests.forEach((test: ITest) => {
-					setTabs(tabs => [
-						...tabs,
-						<Test getMaterials={getMaterials} test={test} isTest={true} />,
-					])
-				})
-			})
-			.catch((err: AxiosError) => {
-				setError(err)
-			})
-		// getTopic
-		axiosInstance
-			.get(`/topic/getTopic/${params.id}`)
-			.then(res => {
-				setTopic(res.data.message)
-				console.log(res.data.message)
-			})
-			.catch((err: AxiosError) => {
-				setError(err)
-			})
-	}
+      axiosInstance
+         .get(`/topic/getMaterials/${params.id}`)
+         .then(res => {
+            setTopic(res.data.message)
+            setTabs([])
+            console.log(res.data.message)
+            res.data.message.lectures.forEach((lecture: ILecture) => {
+               setTabs(tabs => [
+                  ...tabs,
+                  <Lection
+                     lecture={lecture}
+                     SetLectionUpdateOpen={setLectionUpdateOpen}
+                     setLectionUpdate={setLectionUpdate}
+                     isTest={false}
+                     getMaterials={getMaterials}
+                  />,
+               ])
+            })
+            res.data.message.tests.forEach((test: ITest) => {
+               setTabs(tabs => [
+                  ...tabs,
+                  <Test getMaterials={getMaterials} test={test} isTest={true} isTestUpdateOpen={isTestUpdateOpen} setTestUpdateOpen={setTestUpdateOpen} />,
+               ])
+            })
+         })
+         .catch((err: AxiosError) => {
+            setError(err)
+         })
+      // getTopic
+      axiosInstance
+         .get(`/topic/getTopic/${params.id}`)
+         .then(res => {
+            setTopic(res.data.message)
+            console.log(res.data.message)
+         })
+         .catch((err: AxiosError) => {
+            setError(err)
+         })
+   }
 
-	useEffect(() => {
-		// getMaterials
-		getMaterials()
-		// getTopic
-		axiosInstance
-			.get(`/topic/getTopic/${params.id}`)
-			.then(res => {
-				setTopic(res.data.message)
-				console.log(res.data.message)
-			})
-			.catch((err: AxiosError) => {
-				setError(err)
-			})
-	}, [])
+   useEffect(() => {
+      // getMaterials
+      getMaterials()
+      // getTopic
+      axiosInstance
+         .get(`/topic/getTopic/${params.id}`)
+         .then(res => {
+            setTopic(res.data.message)
+            console.log(res.data.message)
+         })
+         .catch((err: AxiosError) => {
+            setError(err)
+         })
+   }, [])
 
-	const [isAddOpen, setAddOpen] = useState(false)
-	const [selectedTab, setSelectedTab] = useState<number>(0)
-	return (
-		<div className='h-screen w-[1080px] mx-auto'>
-			<div className='p-[25px]'>
-				{/* title */}
-				<div className='mb-[25px] flex items-center'>
-					<div className='mr-[15px]'>
-						<Link href={'/profile'}>
-							<Image
-								src={arrow}
-								width={26}
-								height={32}
-								alt='go back arrow'
-								className='cursor-pointer transition-[0.3s] hover:brightness-75 hover:transition-[0.3s]'
-								onClick={() => router.push('profile')}
-							/>
-						</Link>
-					</div>
-					<div className='text-[35px]'>
-						{topic ? topic.title : 'Загрузка...'}
-					</div>
-				</div>
-				<BreadCrumps tab={tabs[selectedTab]} />
-				<div className='flex'>
-					{/* left */}
-					<div className='flex flex-col relative mr-[25px]'>
-						<div className='p-[25px] w-[320px] h-[725px] bg-purple rounded-[22px_22px_0px_0px]'>
-							<div className='*:mb-[5px]'>
-								{tabs.map((tab: React.JSX.Element, index: number) => (
-									<div
-										key={index}
-										className='hover:bg-buttonsHover rounded-[10px] transition-[0.3s] cursor-pointer'
-										onClick={e => {
-											e.preventDefault()
-											console.log(tabs)
+   const [isAddOpen, setAddOpen] = useState(false)
+   const [selectedTab, setSelectedTab] = useState<number>(0)
+   return (
+      <div className='h-screen w-[1080px] mx-auto'>
+         <div className='p-[25px]'>
+            {/* title */}
+            <div className='mb-[25px] flex items-center'>
+               <div className='mr-[15px]'>
+                  <Link href={'/profile'}>
+                     <Image
+                        src={arrow}
+                        width={26}
+                        height={32}
+                        alt='go back arrow'
+                        className='cursor-pointer transition-[0.3s] hover:brightness-75 hover:transition-[0.3s]'
+                        onClick={() => router.push('profile')}
+                     />
+                  </Link>
+               </div>
+               <div className='text-[35px]'>
+                  {topic ? topic.title : 'Загрузка...'}
+               </div>
+            </div>
+            <BreadCrumps tab={tabs[selectedTab]} />
+            <div className='flex'>
+               {/* left */}
+               <div className='flex flex-col relative mr-[25px]'>
+                  <div className='p-[25px] w-[320px] h-[725px] bg-purple rounded-[22px_22px_0px_0px]'>
+                     <div className='*:mb-[5px]'>
+                        {tabs.map((tab: React.JSX.Element, index: number) => (
+                           <div
+                              key={index}
+                              className='hover:bg-buttonsHover rounded-[10px] transition-[0.3s] cursor-pointer'
+                              onClick={e => {
+                                 e.preventDefault()
+                                 console.log(tabs)
 
-											setSelectedTab(index)
-										}}
-									>
-										<div className='p-[10px] text-[15px]'>
-											{!tab.props.isTest
-												? tab.props.lecture.title
-												: 'Тест: ' + tab.props.test.title}
-										</div>
-									</div>
-								))}
-							</div>
-						</div>
-						{isAddOpen ? (
-							<div className='flex justify-between items-center bg-background absolute w-full h-[50px] text-[15px] left-0 bottom-[75px]'>
-								<div
-									className='h-full flex items-center justify-center w-full transition-[0.3s] hover:bg-buttonsHover hover:transition-[0.3s] cursor-pointer'
-									onClick={() => {
-										setLectionCreateOpen(true)
-										setAddOpen(false)
-									}}
-								>
-									Лекция
-								</div>
-								<span className='border-l-[2px] border-solid border-t-white h-full'></span>
-								<div
-									className='flex items-center justify-center h-full w-full transition-[0.3s] hover:bg-buttonsHover hover:transition-[0.3s] cursor-pointer'
-									onClick={() => {
-										setTestCreateOpen(true)
-										setAddOpen(false)
-									}}
-								>
-									Тест
-								</div>
-							</div>
-						) : (
-							''
-						)}
-						<div
-							className='flex justify-center items-center cursor-pointer w-full bg-lightPurple transition-[0.3s] hover:bg-buttonsHover hover:transition-[0.3s] h-[75px] rounded-[0px_0px_22px_22px] text-[25px]'
-							onClick={() => setAddOpen(!isAddOpen)}
-						>
-							Добавить
-						</div>
-					</div>
-					{/* right */}
-					<div className='w-[685px] relative p-[25px] h-[800px] bg-purple rounded-[22px]'>
-						{isLectionUpdateOpen ? (
-							<LectionEditor
-								setLectionUpdateOpen={setLectionUpdateOpen}
-								setLectionCreateOpen={setLectionCreateOpen}
-								update={true}
-								lecture={lectionUpdate!}
-								topic_id={+params.id}
-								getMaterials={getMaterials}
-							/>
-						) : isLectionCreateOpen ? (
-							<LectionEditor
-								setLectionUpdateOpen={setLectionUpdateOpen}
-								setLectionCreateOpen={setLectionCreateOpen}
-								update={false}
-								topic_id={+params.id}
-								getMaterials={getMaterials}
-							/>
-						) : isTestCreateOpen ? (
-							<TestEditor
-								topic_id={+params.id}
-								setTestCreateOpen={setTestCreateOpen}
-								getMaterials={getMaterials}
-							/>
-						) : (
-							tabs[selectedTab]
-						)}
-					</div>
-				</div>
-			</div>
-		</div>
-	)
+                                 setSelectedTab(index)
+                              }}
+                           >
+                              <div className='p-[10px] text-[15px]'>
+                                 {!tab.props.isTest
+                                    ? tab.props.lecture.title
+                                    : 'Тест: ' + tab.props.test.title}
+                              </div>
+                           </div>
+                        ))}
+                     </div>
+                  </div>
+                  {isAddOpen ? (
+                     <div className='flex justify-between items-center bg-background absolute w-full h-[50px] text-[15px] left-0 bottom-[75px]'>
+                        <div
+                           className='h-full flex items-center justify-center w-full transition-[0.3s] hover:bg-buttonsHover hover:transition-[0.3s] cursor-pointer'
+                           onClick={() => {
+                              setLectionCreateOpen(true)
+                              setAddOpen(false)
+                           }}
+                        >
+                           Лекция
+                        </div>
+                        <span className='border-l-[2px] border-solid border-t-white h-full'></span>
+                        <div
+                           className='flex items-center justify-center h-full w-full transition-[0.3s] hover:bg-buttonsHover hover:transition-[0.3s] cursor-pointer'
+                           onClick={() => {
+                              setTestCreateOpen(true)
+                              setAddOpen(false)
+                           }}
+                        >
+                           Тест
+                        </div>
+                     </div>
+                  ) : (
+                     ''
+                  )}
+                  <div
+                     className='flex justify-center items-center cursor-pointer w-full bg-lightPurple transition-[0.3s] hover:bg-buttonsHover hover:transition-[0.3s] h-[75px] rounded-[0px_0px_22px_22px] text-[25px]'
+                     onClick={() => setAddOpen(!isAddOpen)}
+                  >
+                     Добавить
+                  </div>
+               </div>
+               {/* right */}
+               <div className='w-[685px] relative p-[25px] h-[800px] bg-purple rounded-[22px]'>
+                  {isLectionUpdateOpen ? (
+                     <LectionEditor
+                        setLectionUpdateOpen={setLectionUpdateOpen}
+                        setLectionCreateOpen={setLectionCreateOpen}
+                        update={true}
+                        lecture={lectionUpdate!}
+                        topic_id={+params.id}
+                        getMaterials={getMaterials}
+                     />
+                  ) : isLectionCreateOpen ? (
+                     <LectionEditor
+                        setLectionUpdateOpen={setLectionUpdateOpen}
+                        setLectionCreateOpen={setLectionCreateOpen}
+                        update={false}
+                        topic_id={+params.id}
+                        getMaterials={getMaterials}
+                     />
+                  ) : isTestCreateOpen ? (
+                     <TestEditor
+                        topic_id={+params.id}
+                        setTestCreateOpen={setTestCreateOpen}
+                        getMaterials={getMaterials}
+                     />
+                  ) :
+                     isTestUpdateOpen ? (
+                        <TestEditor
+                           topic_id={+params.id}
+                           setTestCreateOpen={setTestCreateOpen}
+                           getMaterials={getMaterials}
+
+                        />
+                     )
+                        : (
+                           tabs[selectedTab]
+                        )}
+               </div>
+            </div>
+         </div>
+      </div>
+   )
 }
 
 export default Topic
--- client/components/teacher/topic/Test.tsx
+++ client/components/teacher/topic/Test.tsx
@@ -4,81 +4,92 @@ import { ITest } from '@/types/models/ITest'
 import { TQuestionData } from '@/types/TQuestionData'
 import axiosInstance from '@/utils/axiosInstance'
 import { AxiosError } from 'axios'
-import { useEffect, useState } from 'react'
+import { Dispatch, SetStateAction, useEffect, useState } from 'react'
 import Question from './Question'
 import StudentsAnswers from './StudentsAnswer'
 
 interface IProps {
-	test: ITest
-	isTest: boolean
-	getMaterials: () => void
+   test: ITest
+   isTest: boolean
+   getMaterials: () => void
+   isTestUpdateOpen: boolean
+   setTestUpdateOpen: Dispatch<SetStateAction<boolean>>
 }
 
 const Test = (props: IProps) => {
-	const [isStudentsAnswersOpen, setStudentsAnswersOpen] =
-		useState<boolean>(false)
-	const [testData, setTestData] = useState<any>(null)
-	const [error, setError] = useState<AxiosError | null>(null)
-	const [questionData, setQuestionData] = useState<TQuestionData[]>([])
+   const [isStudentsAnswersOpen, setStudentsAnswersOpen] =
+      useState<boolean>(false)
+   const [testData, setTestData] = useState<any>(null)
+   const [error, setError] = useState<AxiosError | null>(null)
+   const [questionData, setQuestionData] = useState<TQuestionData[]>([])
 
-	useEffect(() => {
-		// getTest
-		axiosInstance
-			.get(`/topic/getTest/${props.test.id}`)
-			.then(res => {
-				setTestData(res.data.message)
-				console.log(res.data.message)
-			})
-			.catch(err => {
-				setError(err)
-			})
-	}, [props.test])
+   useEffect(() => {
+      // getTest
+      axiosInstance
+         .get(`/topic/getTest/${props.test.id}`)
+         .then(res => {
+            setTestData(res.data.message)
+            console.log(res.data.message)
+         })
+         .catch(err => {
+            setError(err)
+         })
+   }, [props.test])
 
-	const deleteTest = () => {
-		axiosInstance.delete(`/topic/deleteTest/${props.test.id}`)
-		props.getMaterials()
-	}
-	return (
-		<>
-			{isStudentsAnswersOpen ? (
-				<StudentsAnswers
-					SetStudentAnswersOpen={setStudentsAnswersOpen}
-					test_id={props.test.id}
-				/>
-			) : (
-				<div className='flex flex-col justify-between h-full'>
-					<div>
-						<div className='font-bold text-[23px] mb-[15px]'>
-							Тест: {props.test.title}
-						</div>
-						{testData &&
-							testData.questions?.map((q: IQuestion) => (
-								<Question
-									setQuestionData={setQuestionData}
-									questionData={questionData}
-									key={q.id}
-									question={q}
-								/>
-							))}
-					</div>
-					<div className='flex leading-[3px] text-[15px] self-end mt-[25px]'>
-						<button
-							className='bg-lightPurple transition-[0.3s] hover:bg-buttonsHover hover:transition-[0.3s] w-[230px] h-[45px] flex justify-center items-center rounded-[22px] mr-[10px]'
-							onClick={() => setStudentsAnswersOpen(true)}
-						>
-							Результаты прохождения
-						</button>
-						<button
-							className=' bg-lightPurple transition-[0.3s] hover:bg-buttonsHover hover:transition-[0.3s] w-[130px] h-[45px] flex justify-center items-center rounded-[22px]'
-							onClick={() => deleteTest()}
-						>
-							Удалить
-						</button>
-					</div>
-				</div>
-			)}
-		</>
-	)
+   const deleteTest = () => {
+      axiosInstance.delete(`/topic/deleteTest/${props.test.id}`)
+      props.getMaterials()
+   }
+   const updateTest = () => {
+      props.setTestUpdateOpen(true)
+   }
+   return (
+      <>
+         {isStudentsAnswersOpen ? (
+            <StudentsAnswers
+               SetStudentAnswersOpen={setStudentsAnswersOpen}
+               test_id={props.test.id}
+            />
+         ) : (
+            <div className='flex flex-col justify-between h-full'>
+               <div>
+                  <div className='font-bold text-[23px] mb-[15px]'>
+                     Тест: {props.test.title}
+                  </div>
+                  {testData &&
+                     testData.questions?.map((q: IQuestion) => (
+                        <Question
+                           setQuestionData={setQuestionData}
+                           questionData={questionData}
+                           key={q.id}
+                           question={q}
+                        />
+                     ))}
+               </div>
+               <div className='flex leading-[3px] text-[15px] self-end mt-[25px]'>
+                  <button
+                     className='bg-lightPurple transition-[0.3s] hover:bg-buttonsHover hover:transition-[0.3s] w-[230px] h-[45px] flex justify-center items-center rounded-[22px] mr-[10px]'
+                     onClick={() => setStudentsAnswersOpen(true)}
+                  >
+                     Результаты прохождения
+                  </button>
+                  <button
+                     className='bg-lightPurple transition-[0.3s] hover:bg-buttonsHover mr-[10px] hover:transition-[0.3s] w-[130px] h-[45px] flex justify-center items-center rounded-[22px]'
+                     onClick={() => updateTest()}
+                  >
+                     Редактитровать
+                  </button>
+                  <button
+                     className='bg-lightPurple transition-[0.3s] hover:bg-buttonsHover hover:transition-[0.3s] w-[130px] h-[45px] flex justify-center items-center rounded-[22px]'
+                     onClick={() => deleteTest()}
+                  >
+                     Удалить
+                  </button>
+               </div>
+            </div>
+         )}
+      </>
+   )
 }
 
 export default Test
